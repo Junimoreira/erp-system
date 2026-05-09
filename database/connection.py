64863a30ff11e7e9faa35ba136beadd9fbbd006d
junimoreira <moreiraelcio32@gmail.com>
2026-05-09 00:43:07 -0300

    corrige conexão

--- database/connection.py
+++ database/connection.py
@@ -1,20 +1,4 @@
 import psycopg2
-#import os
-
-#from dotenv import load_dotenv
-
-#load_dotenv()
-
-#def conectar():
-
-    #conn = psycopg2.connect(
-      #  os.getenv("DATABASE_URL"),
-     #   sslmode="require"
-    #)
-
-    #return conn
-
-import psycopg2
 import os
 from dotenv import load_dotenv
 
@@ -24,11 +8,12 @@ def conectar():
 
     database_url = os.getenv("DATABASE_URL")
 
-    print("DATABASE_URL:", database_url)
+    print("DATABASE_URL =", database_url)
 
-    conn = psycopg2.connect(
+    if not database_url:
+        raise Exception("DATABASE_URL não encontrada")
+
+    return psycopg2.connect(
         database_url,
         sslmode="require"
     )
-
-    return conn
