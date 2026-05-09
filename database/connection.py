488d7749e3709b16c948492a76f0b24329e60b1a
junimoreira <moreiraelcio32@gmail.com>
2026-05-09 00:55:13 -0300

    ajustanto banco de dados

--- database/connection.py
+++ database/connection.py
@@ -8,7 +8,7 @@ def conectar():
 
     database_url = os.getenv("DATABASE_URL")
 
-    print("DATABASE_URL =", database_url)
+    print("DATABASE_URL =", postgresql://bd_erp_system_user:QEEydMjHue8CdduUnbcjOnSdKp7dQSbG@dpg-d7uc7aho3t8c73fao0rg-a/bd_erp_system
 
     if not database_url:
         raise Exception("DATABASE_URL não encontrada")
@@ -17,3 +17,4 @@ def conectar():
         database_url,
         sslmode="require"
     )
+
