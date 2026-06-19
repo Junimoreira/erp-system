bfe52760dd589431562a7c4e121e5d10a2b1562c
junimoreira <moreiraelcio32@gmail.com>
2026-06-19 20:13:32 -0300

    Corrige import xml_import_db em compras

--- telas/compras.py
+++ telas/compras.py
@@ -3,8 +3,6 @@ import pandas as pd
 
 from database.connection import conectar
 from services.xml_nfe_service import ler_xml_nfe
-from database.xml_import_db import importar_nfe_xml
-#from services.xml_nfe_service import ler_xml_nfe
 from services.xml_import_db import importar_nfe_xml
 
 from database.compras_db import (
